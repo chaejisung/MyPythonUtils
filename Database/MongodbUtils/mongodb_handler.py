060ef9f4893411aed56fb4c4921e606d9cb780db
cjs <jschae02@khu.ac.kr>
2024-07-26 15:01:12 +0900

    feat: CRUD method add

--- Database/MongodbUtils/mongodb_handler.py
+++ Database/MongodbUtils/mongodb_handler.py
@@ -1,5 +1,6 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-from typing import Optional
+from typing import Optional, Union, Dict, List
+from bson import ObjectId
 import asyncio
 
 from settings import settings, Settings
@@ -10,11 +11,13 @@ class MongoDBHandler():
     db_conn = None
     
     # 싱글톤 객체 생성을 위한 __new__ 오버라이드
+    @classmethod
     def __new__(cls, *args, **kwargs):
         if(cls.instacne is None):
             cls.instance = super(MongoDBHandler, cls).__new__(cls, *args, **kwargs)
         return cls.instance
 
+    # 컬렉션 연결용 init
     def __init__(self, mongodb_settings:Settings=settings, coll_config:Optional[dict]=None)->None:
         if(MongoDBHandler.db_conn is None):    
             host = mongodb_settings.MONGODB_HOST
@@ -36,8 +39,90 @@ class MongoDBHandler():
             try:
                 self.db_coll = MongoDBHandler.db_conn[db_name][coll_name]
             except Exception as e:
-                print(f"MongoDBHandler Errer: {e}")
-                
+                print(f"MongoDBHandler Error: {e}")
+    
+    # 연결 해제         
     def close(self):
         if(MongoDBHandler.db_conn is not None):
             MongoDBHandler.db_conn.close()
+            
+    # Create => insert
+    async def insert(self, documents:Union[Dict[str, str], list[Dict[str, str]]])->Union[ObjectId, List[ObjectId], bool]:
+        try:
+            # 하나의 객체만 삽입 -> ObjectId 반환
+            if(type(documents) is dict):
+                result = await self.db_coll.insert_one(documents)
+                return result.inserted_id
+            # 여러 객체 삽입 -> ObjectId 배열 반환
+            elif(type(documents) is list):
+                result = await self.db_coll.insert_many
+                result_list = [id for id in result.values()]
+                return result_list
+        # 오류는 False 반환
+        except Exception as e:
+            print(f"MongoDBHandler Insert Error: {e}")
+            return False
+    
+    # Read => select
+    async def select(self, 
+                     filter:Dict={},
+                     projection:Optional[Dict[str, str]]=None)->Union[Dict, List[Dict], bool]:
+        try:
+            # _id 기준으로 검색 시 1개 반환, dict
+            if(filter.get("_id", None) is not None):
+                result = await self.db_coll.find_one(filter, projection)
+                # 없으면 오류 반환
+                if(result is None):
+                    raise ValueError("Cannot find data from collection")
+            # 여러개 반환, dict 배열
+            else:
+                cursor = self.db_coll.find(filter, projection)
+                result_list = await cursor.to_list()
+                await cursor.close()
+                return result_list
+                
+        # 오류는 False 반환
+        except Exception as e:
+            print(f"MongoDBHandler Select Error: {e}")
+            return False
+        
+    # update => update
+    async def update(self, 
+                     filter:Dict={},
+                     update:Dict=None)->Union[int, bool]:
+        try:
+            # _id 기준으로 검색 시 1개 수정, dict
+            if(filter.get("_id", None) is not None):
+                result = await self.db_coll.update_one(filter, update)
+            # 여러 개 수정
+            else:
+                result = await self.db_coll.update_many(filter, update)
+            
+            updated_count = result.modified_count
+            if(updated_count == 0):
+                raise ValueError("Cannot find data from collection")
+            return updated_count    
+        
+        # 오류는 False 반환
+        except Exception as e:
+            print(f"MongoDBHandler Update Error: {e}")
+            return False
+        
+    # Delete => delete
+    async def delete(self, filter:Dict={})->Union[int, bool]:
+        try:
+            # _id 기준으로 검색 시 1개 삭제, dict
+            if(filter.get("_id", None) is not None):
+                result = await self.db_coll.delete_one(filter)
+            else:
+                result = await self.db_coll.delete_many(filter)
+            
+            deleted_count = result.deleted_count
+            if(deleted_count == 0):
+                raise ValueError("Cannot find data from collection")
+            return deleted_count
+        
+        # 오류는 False 반환
+        except Exception as e:
+            print(f"MongoDBHandler Delete Error: {e}")
+            return False
